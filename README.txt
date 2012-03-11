132c456416368435961d9667b15ea610f53aaba5
Mateus Reis <mateus_reis@yahoo.com>
2012-03-11 15:18:44 -0300

    primeira versao

---
+++ README.txt
@@ -0,0 +1,11 @@
+Software Emissor NF-e - Versão de produção (com VALIDADE JURÍDICA)
+http://www.emissornfe.fazenda.sp.gov.br/
+
+
+http://code.google.com/p/formy-css-framework/
+
+git@github.com:laboratorio72/small.git
+
+https://github.com/laboratorio72/small
+
+CNPJ:
---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+/*-------------------------------------------------------------------- 
+ * JS Scripts
+ * by:
+   Mateus Pereira (mateus@laboratorio72.com)
+   http://www.laboratorio72.com
+ *
+ * Description: 
+ * Dependencies: 
+ * Usage Example:  
+ * Version: 
+ * Changelog:
+ * 02.09.2010 initial Version 1.0
+--------------------------------------------------------------------*/
+	
+function xxxx(){
+	alert('xxxx');
+}   
